af49d45a31c81e9fe694a806e55428bf465b4502
ieeah <lbnfnc90@gmail.com>
2021-12-17 16:13:04 +0100

    Snack 5 done

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-	<h3> snack 3 </h3>
+	<h3>Snack 3 </h3>
 	<?php
 
 	// SNACK 3
@@ -67,20 +67,32 @@
 	}
 	?>
 
-	<h3> snack 4 </h3>
+	<h3>Snack 4 </h3>
 
 	<?php
-		//		Snack 4
-		// Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta
-		$numbers = [];
-		while (count($numbers) < 15) {
-			$n = rand(1, 15);
-			if (!in_array($n, $numbers)) {
+	//		Snack 4
+	// Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta
+	$numbers = [];
+	while (count($numbers) < 15) {
+		$n = rand(1, 15);
+		if (!in_array($n, $numbers)) {
 			$numbers[] = $n;
-			}
 		}
+	}
 
-		echo var_dump($numbers);
+	echo var_dump($numbers);
+	?>
+
+	<h3>Snack 5</h3>
+	<!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+	<?php
+		$p = 'In at ultricies odio. Vivamus interdum urna enim, ac condimentum lorem euismod et. Nunc quis feugiat ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur ut massa at diam tincidunt euismod sed pulvinar leo. Vivamus mi est, dapibus sit amet lorem a, viverra vestibulum quam. Maecenas consequat egestas neque nec rutrum. Proin id magna a eros ultricies ultrices. Sed purus justo, ultrices vel lacus eu, semper maximus ex. Donec facilisis ante nec lacus vestibulum blandit. Duis cursus varius purus eu placerat. Cras ut magna sem. Fusce imperdiet velit ac ultricies tincidunt. Nulla efficitur aliquet velit, in sagittis mauris. Vestibulum iaculis scelerisque tellus et laoreet. Nullam consequat felis sed lorem egestas iaculis. Orci varius natoque.';
+
+		$ps = explode('.', $p);
+
+		for ($i = 0; $i < count($ps); $i++) {
+			echo $ps[$i] . '<br><br>';
+		}
 	?>
 
 </body>
